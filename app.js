82149f9a16217ebea512d0bca97281b232beaf06
andreyaksenov <andrey.aksenov@jetbrains.com>
2020-04-09 15:04:49 +0300

    Add the capability to remove books

--- app.js
+++ app.js
@@ -35,6 +35,12 @@ UI.prototype.showAlert = function(message, className) {
     }, 3000);
 }
 
+UI.prototype.deleteBook = function(target) {
+    if(target.className === 'delete') {
+        target.parentElement.parentElement.remove();
+    }
+}
+
 UI.prototype.clearFields = function() {
     document.getElementById('title').value = '';
     document.getElementById('author').value = '';
@@ -42,7 +48,7 @@ UI.prototype.clearFields = function() {
 }
 
 
-
+// Event listener for adding a book
 document.getElementById('book-form').addEventListener('submit', function (e) {
     const title = document.getElementById('title').value,
         author = document.getElementById('author').value,
@@ -58,6 +64,16 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
         ui.showAlert('Book added!', 'success');
         ui.clearFields();
     }
+
+    e.preventDefault();
+});
+
+
+// Event listener for deleting a book
+document.getElementById('book-list').addEventListener('click', function (e) {
+    const ui = new UI();
+    ui.deleteBook(e.target);
+    ui.showAlert('Book removed!', 'success');
 
     e.preventDefault();
 });
